4ceb6ec5bd831aa7e0846ce63ff72d45088263c1
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2021-09-23 17:52:09 +0300

    fix

--- car/views.py
+++ car/views.py
@@ -1,13 +1,12 @@
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.parsers import MultiPartParser
 
 from .models import CarModel
 from .serializers import CarSerializer
 
+
 class MyView(ListCreateAPIView):
     queryset = CarModel.objects.all()
     serializer_class = CarSerializer
-    parser_classes = (MultiPartParser,)
 
     def get_serializer_context(self):
         return {'request': self.request}
